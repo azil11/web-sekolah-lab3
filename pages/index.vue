8ac2cd5c21e29c4aad43227b6747b151b7400cc1
xyazil_ <146702682+azil11@users.noreply.github.com>
2025-01-03 13:37:01 +0700

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,81 @@
+<template>
+      <div id="carouselExampleDark" class="carousel carousel-dark slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="../assets/img/SC.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="../assets/img/guru.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/img/kepsek2.jpg" class="img" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<div class="card" style="width: 20rem;">
+  <img src="../assets/img/kurniawan.png" class="card-img-center" alt="...">
+  <div class="card-body">
+    <p class="card-text"><h4>Kepala Sekolah</h4></p>
+  </div>
+</div>
+  <div class="row">
+        <h3>JURUSAN</h3>
+        <div class="col-6">
+            <img src="~/assets/img/tjkt.jpg" alt="">
+            <img src="~/assets/img/pplg.jpg" alt="">
+            <img src="~/assets/img/tbsm.jpg" alt="">
+            <img src="~/assets/img/dkv.jpg" alt="">
+            <img src="~/assets/img/toi.jpg" alt="">
+        </div> 
+    </div>
+</template>
+<style>
+.carousel-item img{
+  height: 90vh;
+  width: 100vw;
+}
+.card{
+  margin: 400px;
+  display: flex;
+  justify-content: center;
+}
+.row{
+  text-align: center ;
+}
+.image-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr); 
+  grid-template-rows: repeat(2, auto); 
+  gap: 10px; 
+}
+
+.image-grid img {
+  width: 100%;
+  height: auto;
+}
+</style>
---
+++ pages/profile/visi.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-11 border-dark">
+
+
+                <h1>Visi & Misi SMK Negeri 4 Tasikmalaya</h1>
+
+                <h3> Visi :</h3>
+
+                TERWUJUDNYA LULUSAN YANG CERDAS, AKTIF, KOMPETITIF, ADAPTIF, DAN PRODUKTIF BERLANDASKAN IMAN DAN TAKWA
+
+                (CAKAP BERIMTAK)
+
+
+                <h3>Misi :</h3>
+                Menyiapkan Sumber Daya Manusia yang cerdas dan kompeten baik hardskill maupun softskill.
+                Meningkatkan dan mengembangkan aktifitas serta kreatifitas seluruh warga sekolah dalam berbagai kegiatan
+                positif.
+                Mewujudkan manajemen pengelolaan yang efektif, efesien, transparan, akuntabel dan layanan prima.
+                Menumbuhkan dan mengembangkan potensi dan kemampuan Sumber Daya Manusia yang berdaya saing tinggi
+                melalui berbagai kegiatan akademik maupun non akademik.
+                Menyelenggarakan berbagai program kegiatan dalam upaya meningkatkan Sumber Daya Manusia yang mampu
+                menyesuaikan dengan perkembangan.
+                Mengembangkan sarana prasarana dan lingkungan sekolah yang menyenangkan sebagai wadah menumbuhkembangkan
+                daya kreasi dan inovasi untuk menghasilkan produk teknologi tepat guna.
+                Membangun jiwa wirausaha yang handal melalui pembelajaran Teaching Factory (TEFA) dan Kelas Industri.
+                Menjalin dan mengembangkan kemitraan dengan Industri dan Dunia Kerja (IDUKA) serta lembaga lainnya yang
+                relevan.
+                Menanamkan dan membudayakan sikap dan perilaku yang baik pada aktivitas di sekolah maupun dalam
+                kehidupan sehari-hari.
+
+            </div>
+        </div>
+    </div>
+</template>
